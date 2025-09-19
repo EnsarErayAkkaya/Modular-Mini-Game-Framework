d2e53fe3d9fa0322dec843cf806675b2077daeed
Ensar Eray Akkaya <33553090+EnsarErayAkkaya@users.noreply.github.com>
2025-09-19 23:06:47 +0300

    Update HoopBallGame.cs

--- Assets/Scripts/Runtime/MiniGames/HoopBall/HoopBallGame.cs
+++ Assets/Scripts/Runtime/MiniGames/HoopBall/HoopBallGame.cs
@@ -81,6 +81,7 @@ namespace EEA.MiniGames.HoopBall
 
         public void EndGame()
         {
+            _eventBus.Clear();
             ServicesContainer.EventBus.Publish(new OnMiniGameEnded(_hoopBallData.Id));
         }
 
